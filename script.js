c019073361da98326023747f0b4212b0938b3720
georgina puig <georginaxcodes@gmail.com>
2020-08-03 17:00:00 -0500

    desktop and mobile version

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const code = document.querySelector('.code');
 const key = document.querySelector('.key');
+const pressKey = document.querySelector('.press-key');
+const input = document.getElementById('input');
 
-key.innerHTML = 'Press a key';
+if (window.screen.width >= 1024 && window.screen.height >= 768) {
+  pressKey.style.display = 'block';
+  pressKey.innerHTML = 'Press a key';
+} else {
+  input.style.display = 'block';
+}
 
 document.addEventListener('keyup', (e) => {
   console.log(e.key);
-  key.innerHTML = e.key;
+  input.value = '';
+  pressKey.style.display = 'none';
   code.innerHTML = e.keyCode;
+
+  // letter
+  if (e.key) {
+    key.style.display = 'block';
+    key.innerHTML = e.key;
+  }
 });
+
