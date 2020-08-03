c58f51b3e57cf25d852bff2828ea16a1a606eb59
georgina puig <georginaxcodes@gmail.com>
2020-08-03 00:49:14 -0500

    fix bugs

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const code = document.querySelector('.code');
 const key = document.querySelector('.key');
 
+key.innerHTML = 'Press a key';
+
 document.addEventListener('keyup', (e) => {
-  console.log(e);
-  code.innerHTML = e.keyCode;
+  console.log(e.key);
   key.innerHTML = e.key;
+  code.innerHTML = e.keyCode;
 });
