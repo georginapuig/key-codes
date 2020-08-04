937fa849fcf0da5d8532254fd70a16369ec70ad9
georgina puig <georginaxcodes@gmail.com>
2020-08-04 02:03:29 -0500

    textinput and keypress

--- script.js
+++ script.js
@@ -10,23 +10,27 @@ if (window.screen.width >= 1024 && window.screen.height >= 768) {
   input.style.display = 'block';
 }
 
+const printLetter = (e) => {
+  if (e.key) {
+    key.style.display = 'block';
+    key.innerHTML = e.key;
+  }
+};
+
 document.addEventListener('textInput', (e) => {
   console.log(e.data.charCodeAt(0));
   input.value = '';
   pressKey.style.display = 'none';
   code.innerHTML = e.data.charCodeAt(0);
+
+  printLetter(e);
 });
 
 document.addEventListener('keypress', (e) => {
   console.log(e.key);
-  input.value = '';
   pressKey.style.display = 'none';
   code.innerHTML = e.keyCode;
 
-  // letter
-  if (e.key) {
-    key.style.display = 'block';
-    key.innerHTML = e.key;
-  }
+  printLetter(e);
 });
 
